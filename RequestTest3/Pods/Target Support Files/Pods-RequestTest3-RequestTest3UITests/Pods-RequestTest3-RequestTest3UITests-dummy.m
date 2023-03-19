93b8eb378c1949c5a65404caf6eacc501cf94348
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-19 12:34:53 -0300

    no message

---
+++ RequestTest3/Pods/Target Support Files/Pods-RequestTest3-RequestTest3UITests/Pods-RequestTest3-RequestTest3UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RequestTest3_RequestTest3UITests : NSObject
+@end
+@implementation PodsDummy_Pods_RequestTest3_RequestTest3UITests
+@end
---
+++ RequestTest3/Pods/Target Support Files/Pods-RequestTest3Tests/Pods-RequestTest3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RequestTest3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RequestTest3Tests
+@end
